ccde76a8b078d6b5fb5a9d016fcc87d04fde87d2
prince-konwea <kokodecontee123@gmail.com>
2023-04-07 10:50:49 +0100

    creating structs for different query params

--- main.go
+++ main.go
@@ -22,6 +22,27 @@ func NewClient(token string) *Client {
 	return &Client{Token: token, hc: c}
 }
 
+type SerachResults struct {
+	Page         int32   `json:"page"`
+	Perpage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results`
+	NextPage     string  `json: "next_page"`
+	Photos       []Photo `json: "photos"`
+}
+
+type Photos struct {
+	Id             int         `json:"id"`
+	Width          int         `json:"width"`
+	Height         int         `json:"height"`
+	Url            string      `json:"url"`
+	Photographer   string      `json:"photographer"`
+	PhotographeUrl string      `json:"photographer_url"`
+	Source         PhotoSource `json: "src"`
+}
+
+type PhotoSource struct {
+}
+
 func main() {
 
 	os.Setenv("pexelsToken", "wDpFgtSdGHlK6PHKZXxfEmb1TVIPJIywEoTyPwlYxRysmlRAGc6gZd9b")
